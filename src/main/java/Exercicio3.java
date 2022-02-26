5a92ca14f9b3e09b25c17439ddc5bb95c2639667
João Pedro Niz de Assis <99514915+Zileey@users.noreply.github.com>
2022-02-25 22:35:29 -0300

    Exercicio 3
    O Exercício 3 ensina como usar o switch para comportamento de vários valores de uma variável

---
+++ src/main/java/Exercicio3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(); // Scanner para entrada de dados
+        int op; // Opção de número de alunos
+        System.out.println("Digite o número de alunos matriculados:");
+        op = sc.nextInt();
+
+        switch (op){ // Switch varia de acordo com o valor da variável op
+            case 10:
+                System.out.println("Sua sala será I-15!");
+                break;
+
+            case 20:
+                System.out.println("Sua sala será I-15!");
+                break;
+
+            case 30: // Aparece quando digita o numero de alunos em 30
+                System.out.println("Sua sala será I-22!");
+                break;
+
+            default: // Aparece quando há qualquer outra opção
+                System.out.println("Opção inválida!");
+                break;
+        }
+
+        sc.close();
+    }
+}
