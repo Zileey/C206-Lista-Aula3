15d0a44f710e3c81f865b9271086a0e1cfca0fc9
João Pedro Niz de Assis <99514915+Zileey@users.noreply.github.com>
2022-02-25 22:11:58 -0300

    Exercício 1
    Exercício 1 fala sobre classes primitivas.

---
+++ src/main/java/Exercicio1.java
@@ -0,0 +1,16 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int partida1 = 120;
+        int partida2 = 240;
+        int partida3 = 320;
+
+        int soma;
+        int media;
+
+        soma = partida1 + partida2 + partida3;
+        media = soma / 3;
+
+        System.out.println("Total de Zumbis: " + soma);
+        System.out.println("Media de Zumbis: "+ media);
+    }
+}
